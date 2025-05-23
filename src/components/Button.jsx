17a3b7e21d5b10c7189c0d2012b3c434c1a5f3be
Administrator <gitlab_admin_1c9a49@example.com>
2025-05-23 01:36:43 -0600

    feat(Home): Se creo la pagina Home y sus respectivos componentes como button y itemCard, se agregaron mas recuross a los assets exportados

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Button = ({ text, imgSrc, imgAlt, className  }) => {
+  return (
+<button className={`ml-10 px-9 py-5 text-gray-500 font-semibold rounded-full transition duration-300 flex items-center transform hover:scale-105 ${className}`}>
+      {text} 
+      <img src={imgSrc} className="pl-5 w-sm hover:stroke-white" alt={imgAlt} />
+    </button>
+  );
+};
+
+export default Button;
