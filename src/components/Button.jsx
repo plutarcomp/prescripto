04a950abb66c0dd49369eae50081af6854d2a4a8
Administrator <gitlab_admin_1c9a49@example.com>
2025-05-27 19:09:42 -0600

    feat(button): Se configuro el link en lso paprametros

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const Button = ({ text, imgSrc, imgAlt, className  }) => {
+const Button = ({ text, imgSrc, imgAlt, className, to  }) => {
   return (
+    <Link to={to}>
 <button className={`ml-10 px-9 py-5 text-gray-500 font-semibold rounded-full transition duration-300 flex items-center transform hover:scale-105 ${className}`}>
       {text} 
       <img src={imgSrc} className="pl-5 w-sm hover:stroke-white" alt={imgAlt} />
     </button>
+    </Link>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
           </h2>
         </div>
         <div className="ml-10">
-          <Button text="Book appointment" imgSrc={assets.arrow_icon} imgAlt="Arrow icon" className="bg-white" />
+          <Button text="Book appointment" imgSrc={assets.arrow_icon} imgAlt="Arrow icon" className="bg-white" to="/Loging" />
         </div>
        
       </div>
@@ -126,7 +126,7 @@ const Home = () => {
       <ItemCard />
 
       <div className="ml-10 flex justify-center">
-        <Button className="pl-20 pr-20 bg-gray-100" text="more" />
+        <Button className="pl-20 pr-20 bg-gray-100" text="more" to="/Loging" />
       </div>
 
     </section>
@@ -141,7 +141,7 @@ const Home = () => {
           </h1>
           
           <div className="ml-10">
-          <Button text="Create account" className="pl-10 bg-white pr-4" />
+          <Button text="Create account" className="pl-10 bg-white pr-4" to="/Loging" />
         </div>
         </div>
 
